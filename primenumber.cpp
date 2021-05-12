ea8a5c193fe77a7fbbd699cf381ff14ee523737a
vaishnavi264 <84063089+vaishnavi264@users.noreply.github.com>
2021-05-12 19:21:18 +0530

    Add files via upload

---
+++ primenumber.cpp
@@ -0,0 +1,65 @@
+/*#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	for(int i=2;i<n-1;i++)
+	{
+		if(n%i==0)
+		{
+			cout<<"it is not prime";
+			break;
+		}
+		else
+		{
+			cout<<"it is prime number";
+			break;
+		}
+	}
+}
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	for(int i=2;i<sqrt(n)+1;i++)
+	{
+		if(n%i==0)
+		{
+			cout<<"it is not prime";
+			break;
+		}
+		else
+		{
+			cout<<"it is prime number";
+			break;
+		}
+	}
+	return 0;
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the number";
+	cin>>n;
+	for(int i=2;i<=(n/2);i++)
+	{
+		if(n%i==0)
+		{
+			cout<<"not a prime";
+			break;
+		}
+		else
+		{
+			cout<<"it is a prime";
+			
+		}
+	}
+}*/
